268f4b1cbf84a802a4505abfedbea435d311740d
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-12-13 11:39:34 -0700

    install semantic UI react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'semantic-ui-css/semantic.min.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>
 )
