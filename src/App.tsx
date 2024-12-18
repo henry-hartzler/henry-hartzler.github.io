8fe7f5729c57c221a32a780ba8285135929d1d7d
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-12-18 15:05:34 -0700

    basic responsive app bar integration for header nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import ResponsiveAppBar from './Components/ResponsiveAppBar'
 
 const darkTheme = createTheme({
 	palette: {
@@ -12,7 +13,9 @@ function App() {
 	return (
 		<ThemeProvider theme={darkTheme}>
 			<CssBaseline />
-			<></>
+			<>
+				<ResponsiveAppBar />
+			</>
 		</ThemeProvider>
 	)
 }
