a56a9c6b093207382173073d8de93560d743fe6a
Henry Hartzler <Henry.Hartzler@schools.utah.gov>
2024-12-18 14:55:30 -0700

    remove template background

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -12,47 +9,10 @@ const darkTheme = createTheme({
 })
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<ThemeProvider theme={darkTheme}>
 			<CssBaseline />
-			<>
-				<div>
-					<a
-						href='https://vite.dev'
-						target='_blank'
-					>
-						<img
-							src={viteLogo}
-							className='logo'
-							alt='Vite logo'
-						/>
-					</a>
-					<a
-						href='https://react.dev'
-						target='_blank'
-					>
-						<img
-							src={reactLogo}
-							className='logo react'
-							alt='React logo'
-						/>
-					</a>
-				</div>
-				<h1>Vite + React</h1>
-				<div className='card'>
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</button>
-					<p>
-						Edit <code>src/App.tsx</code> and save to test HMR
-					</p>
-				</div>
-				<p className='read-the-docs'>
-					Click on the Vite and React logos to learn more
-				</p>
-			</>
+			<></>
 		</ThemeProvider>
 	)
 }
